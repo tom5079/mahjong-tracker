716f20d614e2db3b3f279ea3d107ff43f0f7761a
tom5079 <7948651+tom5079@users.noreply.github.com>
2024-06-21 15:37:24 -0700

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class="flex flex-row items-center justify-between border-b p-8">
+	<h1 class="text-4xl font-extrabold tracking-tight">Mahjong Tracker</h1>
+
+	<div class="flex flex-row space-x-8 p-2">
+		<button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				height="24px"
+				viewBox="0 -960 960 960"
+				width="24px"
+				fill="#5f6368"
+				><path
+					d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"
+				/></svg
+			>
+		</button>
+		<button class="font-semibold">LOGIN</button>
+	</div>
+</nav>
+
+<main>
+	
+</main>
