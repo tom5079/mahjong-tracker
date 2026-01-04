b951c2d536d1f46b4cec473490d183834c3ac534
tom5079 <7948651+tom5079@users.noreply.github.com>
2026-01-04 19:41:56 +0000

    fix avatar

--- src/lib/UserAvatar.svelte
+++ src/lib/UserAvatar.svelte
@@ -8,6 +8,8 @@
 
     let { user, size = 'md' }: Props = $props()
 
+    let isError = $derived(user?.avatar && false)
+
     const sizeToClassMap = {
         sm: 'w-4 h-4',
         md: 'w-8 h-8',
@@ -18,12 +20,12 @@
 {#if user}
     <div class={`overflow-hidden rounded-full ${sizeToClassMap[size]}`}>
         <img
-            src="https://cdn.discordapp.com/avatars/{user.id}/{user.avatar}.webp"
+            src={isError
+                ? 'https://cdn.discordapp.com/emojis/1235123039956500491.webp?size=96'
+                : `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.webp`}
             alt="avatar of {user.username}"
             class="h-full w-full"
-            onerror={(e) =>
-                ((e.currentTarget as HTMLImageElement).src =
-                    'https://cdn.discordapp.com/emojis/1235123039956500491.webp?size=96')}
+            onerror={() => (isError = true)}
         />
     </div>
 {/if}
